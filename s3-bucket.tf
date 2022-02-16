4361da94d141271be413e2ca255889fcc5652bde
n-imai <8196611+n-imai@users.noreply.github.com>
2022-02-16 07:46:07 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,11 +2,5 @@ module "s3_bucket" {
   source  = "app.terraform.io/nimai-training/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = "nimai-training"
-
-  versioning = {
-    enabled = true
-  }
 }
